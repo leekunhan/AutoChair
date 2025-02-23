62f3c6ab39ff7552d52860d63877861b0d56be3e
KH L <kunhanl0511@gmail.com>
2025-02-24 01:46:00 +0800

    add sample website and fastapi function

---
+++ autochair.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+# if not api_key:
+#     raise ValueError("didn't fine OPENAI_API_KEY, please check .env file or environment variables")
+
+# client = OpenAI(api_key=api_key)
+
+# stream = client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[{"role": "user", "content": "Say this is a test"}],
+#     stream=True,
+# )
+
+# for chunk in stream:
+#     if chunk.choices[0].delta.content:
+#         print(chunk.choices[0].delta.content, end="")
+
